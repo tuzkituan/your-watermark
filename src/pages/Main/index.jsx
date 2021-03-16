7af22d8f65b185a77bbd4473296d42a01896a4b5
Lewis Nguyen <lewis.nguyen@terralogic.com>
2021-03-16 11:29:21 +0700

    almost done

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -1,49 +1,71 @@
 import React, { Component } from 'react'
-import IG from '../../assets/ig.png'
-import styles from './index.scss';
+import html2canvas from 'html2canvas'
+import IG from '../../assets/ig.svg'
+import FB from '../../assets/fb.svg'
+import styles from './index.module.scss'
 
 export default class Main extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            text: ''
+            text: '',
+            selectedIcon: 2, // 1 ig, 2 fb,
+            logoSize: 50, // px
+            textSize: 22, // px
         }
+        this.canvasRef = React.createRef();
     }
 
     handleChange = (e) => {
         this.setState({ text: e.target.value });
     }
-    renderMain = () => {
-        const { text } = this.state;
-        return (
-            <div className={styles.mainContainer}>
-                <form>
-                    <label>
-                        Text:
-                    <input type="text" name="text" value={text} onChange={this.handleChange} />
-                    </label>
-                </form>
-            </div>
-        )
-    }
+
     renderImage = () => {
-        const { text } = this.state;
+        const { text, selectedIcon, logoSize, textSize } = this.state;
         return (
-            <div className={styles.exportImageContainer}>
-                <img src={IG} alt='instagram' className={styles.imgLogo} />
-                <div className={styles.textLogo}>
-                    <span className={styles.text}>
-                        {text}
+            <div className={styles.borderContainer}>
+                <div id="canvasRef" ref={this.canvasRef} className={styles.exportImageContainer}>
+                   {selectedIcon === 1 &&  <img src={IG} alt='instagram' className={styles.imgLogo} style={{ width: logoSize, height: logoSize }}/> }
+                   {selectedIcon === 2 &&  <img src={FB} alt='facebook' className={styles.imgLogo} style={{ width: logoSize, height: logoSize }} /> }
+                    <div className={styles.divider} />
+                    <span className={styles.textLogo}>
+                        <span className={styles.text} style={{fontSize: textSize }}>
+                            {text}
+                        </span>
+                    </span>
+                </div>
+                <div className={styles.inputContainer}>
+                    <form>
+                        {/* <label>Text:</label> */}
+                        <input type="text"
+                            value={text}
+                            onChange={this.handleChange}
+                            placeholder="Enter your text here"
+                            className={styles.textInput} />
+                    </form>
+                </div>
+                <div className={styles.exportButtonContainer}>
+                    <span onClick={() => this.printDocument(this.canvasRef.current)}>
+                        DOWNLOAD
                     </span>
                 </div>
             </div>
         )
     }
+
+    printDocument = (domElement) => {
+        html2canvas(domElement).then(canvas => {
+            var a = document.createElement('a');
+            a.href = canvas.toDataURL("image/jpeg").replace("image/jpeg", "image/octet-stream");
+            a.download = 'instagram-logo.jpg';
+            a.click();
+        })
+    }
+
     render() {
         return (
             <div className={styles.Main}>
                 <p className={styles.title}>Your watermark</p>
-                {this.renderMain()}
                 {this.renderImage()}
             </div>
         )
