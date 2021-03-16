85614d04f28741e6082d57a6b95b167f81c45f1a
Lewis Nguyen <lewis.nguyen@terralogic.com>
2021-03-16 13:01:48 +0700

    done UI

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -3,13 +3,14 @@ import html2canvas from 'html2canvas'
 import IG from '../../assets/ig.svg'
 import FB from '../../assets/fb.svg'
 import styles from './index.module.scss'
+import { Link } from 'react-router-dom'
 
 export default class Main extends Component {
     constructor(props) {
         super(props);
         this.state = {
             text: '',
-            selectedIcon: 2, // 1 ig, 2 fb,
+            selectedIcon: 1, // 1 ig, 2 fb,
             logoSize: 50, // px
             textSize: 22, // px
         }
@@ -17,21 +18,40 @@ export default class Main extends Component {
     }
 
     handleChange = (e) => {
-        this.setState({ text: e.target.value });
+        if (e.target.value.length < 30)
+            this.setState({ text: e.target.value });
     }
 
+    handleChangeIcon = (selectedIcon) => {
+        this.setState({
+            selectedIcon
+        })
+    }
     renderImage = () => {
         const { text, selectedIcon, logoSize, textSize } = this.state;
         return (
             <div className={styles.borderContainer}>
-                <div id="canvasRef" ref={this.canvasRef} className={styles.exportImageContainer}>
-                   {selectedIcon === 1 &&  <img src={IG} alt='instagram' className={styles.imgLogo} style={{ width: logoSize, height: logoSize }}/> }
-                   {selectedIcon === 2 &&  <img src={FB} alt='facebook' className={styles.imgLogo} style={{ width: logoSize, height: logoSize }} /> }
-                    <div className={styles.divider} />
-                    <span className={styles.textLogo}>
-                        <span className={styles.text} style={{fontSize: textSize }}>
-                            {text}
+                <div className={styles.exportImageContainer}>
+                    <div id="canvasRef" ref={this.canvasRef} className={styles.imageContainer}>
+                        {selectedIcon === 1 &&
+                            <div className={styles.imgContainer}>
+                                <img src={IG} alt='instagram' className={styles.imgLogo}
+                                    style={{ width: `${logoSize}px`, height: `${logoSize}px` }} /></div>}
+
+                        {selectedIcon === 2 &&
+                            <div className={styles.imgContainer}><img src={FB} alt='facebook' className={styles.imgLogo}
+                                style={{ width: `${logoSize}px`, height: `${logoSize}px` }} /></div>}
+                        {/* <div className={styles.divider} /> */}
+                        <span className={styles.textLogo}>
+                            <span className={styles.text} style={{ fontSize: `${textSize}px` }}>
+                                {text}
+                            </span>
                         </span>
+                    </div>
+                </div>
+                <div className={styles.exportButtonContainer}>
+                    <span onClick={() => this.printDocument(this.canvasRef.current)}>
+                        DOWNLOAD
                     </span>
                 </div>
                 <div className={styles.inputContainer}>
@@ -44,10 +64,51 @@ export default class Main extends Component {
                             className={styles.textInput} />
                     </form>
                 </div>
-                <div className={styles.exportButtonContainer}>
-                    <span onClick={() => this.printDocument(this.canvasRef.current)}>
-                        DOWNLOAD
-                    </span>
+                <div className={styles.iconContainer}>
+                    <div
+                        onClick={() => this.handleChangeIcon(1)}
+                        className={selectedIcon === 1 ? styles.active : ''}>
+                        <img src={IG} alt='instagram'
+                            className={styles.imgIcon} />
+                    </div>
+                    <div
+                        onClick={() => this.handleChangeIcon(2)}
+                        className={selectedIcon === 2 ? styles.active : ''}>
+                        <img src={FB} alt='facebook'
+                            className={styles.imgIcon} />
+                    </div>
+                </div>
+                <div className={styles.optionContainer}>
+                    <div className={styles.iconSize}>
+                        <p>Icon size: {logoSize}px</p>
+                        <input
+                            type="range" min="1" max="50"
+                            value={logoSize}
+                            class={styles.slider}
+                            onChange={(e) => {
+                                this.setState({
+                                    logoSize: e.target.value
+                                })
+                            }}
+                            id="logoSize" />
+                    </div>
+                    <div className={styles.textSize}>
+                        <p>Text size: {textSize}px</p>
+                        <input
+                            type="range" min="1" max="50"
+                            value={textSize}
+                            class={styles.slider}
+                            onChange={(e) => {
+                                this.setState({
+                                    textSize: e.target.value
+                                })
+                            }}
+                            id="textSize" />
+                    </div>
+                </div>
+                <div className={styles.copyrightContainer}>
+                    <p>Made by <a href='https://facebook.com/tuzkituan'>Nguyen Ngoc Tuan <span>(Lewis Nguyen)</span></a></p>
+                    <a href='https://facebook.com/groups/phonephotographyvietnam/'>memories of us - vietnam photography</a>
                 </div>
             </div>
         )
@@ -57,7 +118,7 @@ export default class Main extends Component {
         html2canvas(domElement).then(canvas => {
             var a = document.createElement('a');
             a.href = canvas.toDataURL("image/jpeg").replace("image/jpeg", "image/octet-stream");
-            a.download = 'instagram-logo.jpg';
+            a.download = 'your-watermark.jpg';
             a.click();
         })
     }
